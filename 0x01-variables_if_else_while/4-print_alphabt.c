562880bd7304bc1d94f1da2ba99c1c12d05e31ab
Naol <naolmulatu11@gmail.com>
2022-10-14 02:47:36 -0700

    Commmit 0-5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,43 @@
+nes (17 sloc)  311 Bytes
+
+
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ * * main - Print the alphabet in lowercase letters, except for e and q
+ *
+ * *
+ *
+ * * Return: Always 0 (Success)
+ *
+ * */
+
+int main(void)
+
+{
+
+			char aphabet;
+
+
+
+					for (aphabet = 'a'; aphabet <= 'z'; aphabet++)
+
+								{
+
+											if (aphabet != 'e' && aphabet != 'q')
+
+														putchar(aphabet);
+
+													}
+
+							putchar('\n');
+
+
+
+								return (0);
+
+}
